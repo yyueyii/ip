b9b34dc6b77e4c6edbacfe20320a509eda5f88f3
yyueyii <>
2024-08-21 18:10:26 +0800

    Level 0 completed

---
+++ src/main/java/Mario.java
@@ -0,0 +1,12 @@
+public class Mario {
+    public static void main(String[] args) {
+        String logo = "-------------------------------------\n"
+                + "It's-a me, Mario! \n"
+                + "How can I help you today?\n"
+                + "-------------------------------------\n";
+
+        String goodbye = "Buh-bye, see you soon!\n"
+                + "-------------------------------------\n";
+        System.out.println(logo + goodbye);
+    }
+}
